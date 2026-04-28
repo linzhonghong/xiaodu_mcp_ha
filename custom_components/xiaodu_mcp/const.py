8c935637066832d82ff7bfe7bbe92726101f8978
linzhonghong <215634639@qq.com>
2026-04-28 23:43:10 +0800

    Add custom_components\xiaodu_mcp\const.py

---
+++ custom_components/xiaodu_mcp/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "xiaodu_mcp"
+CONF_BASE_URL = "base_url"
+PLATFORMS = ["sensor", "button", "camera", "text"]
+DATA_CLIENT = "client"
+DATA_COORDINATOR = "coordinator"
+DATA_PHOTOS = "photos"
+DEFAULT_SCAN_INTERVAL_SECONDS = 300
